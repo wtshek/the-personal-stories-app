be8223fcf860ba6dfcaa9176ab0a72ea9af28353
wtshek <wingtung.shek@accedo.tv>
2023-07-15 11:36:16 -0400

    add prisma and config setup

---
+++ src/app/api/setup/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/prisma/prisma";
+import {
+  gender as genderConfig,
+  industries as industriesConfig,
+} from "@/src/utils/config";
+
+export async function GET() {
+  let gender, industries;
+
+  gender = await prisma.gender.findMany();
+  industries = await prisma.industry.findMany();
+
+  // didn't setup database
+  if (!gender.length || !industries.length) {
+    gender = await prisma.gender.createMany({ data: genderConfig });
+    industries = await prisma.industry.createMany({ data: industriesConfig });
+  }
+
+  return NextResponse.json({ gender, industries });
+}
---
+++ src/utils/config.ts
@@ -0,0 +1,39 @@
+export const gender = [
+  {
+    label: "Female",
+  },
+  {
+    label: "Male",
+  },
+];
+
+export const industries = [
+  { label: "Agriculture and Farming" },
+  { label: "Automotive" },
+  { label: "Aviation" },
+  { label: "Banking and Finance" },
+  { label: "Biotechnology" },
+  { label: "Chemicals" },
+  { label: "Construction" },
+  { label: "Consumer Goods" },
+  { label: "Defense" },
+  { label: "Education" },
+  { label: "Energy" },
+  { label: "Entertainment" },
+  { label: "Food and Beverage" },
+  { label: "Gaming" },
+  { label: "Healthcare" },
+  { label: "Hospitality and Tourism" },
+  { label: "Information Technology" },
+  { label: "Insurance" },
+  { label: "Manufacturing" },
+  { label: "Media and Communications" },
+  { label: "Mining and Metals" },
+  { label: "Pharmaceuticals" },
+  { label: "Real Estate" },
+  { label: "Retail" },
+  { label: "Telecommunications" },
+  { label: "Transportation and Logistics" },
+  { label: "Utilities" },
+  { label: "Wholesale Trade" },
+];
